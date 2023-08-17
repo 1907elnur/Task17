fac76e6a17b714f6b52c717e022281b0a63b0418
Elnur Gafarzade <138590291+1907elnur@users.noreply.github.com>
2023-08-17 12:49:35 +0400

    Add files via upload

---
+++ task_homework/task_homework/MainPrintFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_homework
+{
+    internal class MainPrintFile
+    {
+        public void Print(object file) 
+        {
+            if (file is Word)
+            {
+                Console.WriteLine("Word cap edildi");
+            }
+            if (file is Excel )
+            {
+                Console.WriteLine("Excel cap edildi");
+            }
+            if (file is PDF)
+            {
+                Console.WriteLine("PDF cap edildi");
+            }
+        }
+    }
+}
---
+++ task_homework/task_homework/Program.cs
@@ -0,0 +1,8 @@
+﻿using task_homework;
+
+MainPrintFile print = new MainPrintFile();
+Word word = new Word();
+Excel excel = new Excel();
+PDF pdf = new PDF();
+
+print.Print(pdf);
